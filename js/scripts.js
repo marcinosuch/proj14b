794bea6a4615a258336ff99e77b02ebb5f2b5f13
marcinosuch <mar.osuch@gmail.com>
2017-05-25 23:34:37 +0200

    js poczatki

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// scripts.js
+
+var a = prompt('Enter base of triangle');
+	h = prompt('Enter height of triangle');
+	triangleArea = a * h / 2
+	tA = alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+	console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+	name = prompt('Enter your name');
+	alert('Hello, ' + name);
+	console.log('Hello, ' + name);
+
+
+
+//próba kmomentarza
+/*
+komentarz blokowy
+*/
+
+
+
+
+
+
